51c13fb03f5187d66c850e395d0ee8df67e63ddc
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-09 03:16:57 +0900

    opencv-wasmの型定義ファイルをfetchしてmonacoに読み込ませてみる

--- src/module/CodeEditor.tsx
+++ src/module/CodeEditor.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as Monaco from 'monaco-editor';
+//import { cv } from "opencv-wasm";
 
 interface CodeEditorProps {
 	defaultValue?: string,
@@ -10,15 +11,31 @@ interface CodeEditorProps {
 	onTextChange?: Function
 }
 
+const getOpenCVjsTypeFile = (): Promise<string | any> => {
+	//const url = "https://cdn.jsdelivr.net/npm/opencv-wasm@4.3.0-10/types/opencv.ts";
+	const url = "https://raw.githubusercontent.com/DefinitelyTyped/DefinitelyTyped/master/types/leaflet/index.d.ts"
+	return fetch(url).then(response => {
+		return response.text();
+	}).catch(error => {
+		return error;
+	});
+}
+
+getOpenCVjsTypeFile().then(typings => {
+	console.log(typings);
+	Monaco.languages.typescript.javascriptDefaults.addExtraLib(typings);
+})
 const CodeEditor: React.FunctionComponent<CodeEditorProps> = (props) => {
 	const refCodeEditor = React.useRef(null);
 	React.useEffect(() => {
+		// MonacoEditorを作成
 		let editor = Monaco.editor.create(refCodeEditor.current!!, {
 			value: props.defaultValue || 'console.log("Hello, world!");',
 			language: props.language || 'javascript',
 			theme: props.theme || 'vs-dark',
 			minimap: { enabled: false }
 		});
+		// テキストが変わった場合props.onTextChange()を実行
 		editor.onDidChangeModelContent(() => {
 			let text = editor.getValue();
 			if (props.onTextChange !== undefined) {
