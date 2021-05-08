a0c73cb5229c81f5f69054500f0f65dcb4939f10
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-09 04:14:58 +0900

    opencv-reactを導入するためにとりあえず.d.tsファイルを作成

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,21 @@ import {
 	Grommet
 } from 'grommet';
 import { grommet } from "grommet/themes/grommet";
-//import { OpenCvProvider } from "opencv-react";
+import { OpenCvProvider } from "opencv-react";
 
 import AppBar from "./layout/AppBar";
 import MainContent from "./layout/MainContent";
 import CustomFooter from "./layout/CustomFooter";
 
 const App: React.FunctionComponent = () => {
-
+	const [isOpencvLoaded, setIsOpencvLoaded] = React.useState(false);
 	return (
 		<Grommet theme={grommet} full>
-			<Box fill>
+			<OpenCvProvider openCvPath="https://docs.opencv.org/4.5.0/opencv.js" onLoad={() => setIsOpencvLoaded(true)}>
 				<AppBar />
 				<MainContent />
 				<CustomFooter />
-			</Box>
+			</OpenCvProvider>
 		</Grommet>
 	)
 }
