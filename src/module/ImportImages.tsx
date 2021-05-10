b84ef7be0705023f8fb56d4475084637e387ebdd
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-10 19:49:34 +0900

    Update ImportImages.tsx - FileInputによるファイル読み込みをやめ一から記述し直した

--- src/module/ImportImages.tsx
+++ src/module/ImportImages.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import {
 	Box,
 	BoxProps,
-	FileInput,
 	Heading,
 	Grid,
-	Paragraph
+	Paragraph,
+	Text
 } from "grommet";
-
+import {
+	Gallery
+} from "grommet-icons";
 import LoadedImage from "./LoadedImage";
 
 const sampleImages = [
@@ -21,42 +23,88 @@ interface ImportImagesProps {
 	overflow?: BoxProps["overflow"]
 }
 const ImportImages: React.FunctionComponent<ImportImagesProps> = (props) => {
+	// 読み込んだ画像をstateに保存
 	const [images, setImages] = React.useState<{ src: string, name: string, id: string }[]>(sampleImages);
+	// ファイルをドラッグ・アンド・ドロップして読み込む仕組みをつくる
+	const dropAreaRef = React.useRef<HTMLDivElement>(null);
+	const [dragOver, setDragOver] = React.useState(false);
+	const storeImportedImages = (files: FileList): void => {
+		for (let i = 0; i < files.length; i++) {
+			const reader = new FileReader();
+			reader.readAsDataURL(files[i]);
+			reader.onload = () => {
+				if (typeof reader.result === "string") {
+					const newImage = {
+						// @ts-ignore
+						src: reader.result,
+						name: files[i].name,
+						id: Math.random().toString(32).substring(2) // generate random string for accessing each img element
+					};
+					setImages(prevState => prevState.concat([newImage]));
+				}
+			};
+		}
+	};
+	// <input/>がクリックされたとき
 	const onFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		if (event.target.files) {
 			const files = event.target.files;
-			for (let i = 0; i < files.length; i++) {
-				const reader = new FileReader();
-				reader.readAsDataURL(files[i]);
-				reader.onload = () => {
-					if (typeof reader.result === "string") {
-						const newImage = {
-							// @ts-ignore
-							src: reader.result,
-							name: files[i].name,
-							id: Math.random().toString(32).substring(2) // generate random string for accessing each img element
-						}
-						setImages(prevState => prevState.concat([newImage]));
-						console.log(images);
-					}
-				}
-			}
+			storeImportedImages(files);
 		}
-	}
+	};
+	React.useEffect(() => {
+		// 領域にファイルをドロップするとstateに画像を保存
+		dropAreaRef.current?.addEventListener("drop", event => {
+			event.preventDefault();
+			if (event.dataTransfer) {
+				const files = event.dataTransfer.files;
+				storeImportedImages(files);
+			}
+		});
+		// 領域にマウスが乗ったとき背景色を変える
+		dropAreaRef.current?.addEventListener("dragover", event => {
+			event.preventDefault();
+			setDragOver(true);
+		});
+		// 領域から離れたときもとに戻す
+		dropAreaRef.current?.addEventListener("dragleave", event => {
+			event.preventDefault();
+			setDragOver(false);
+		});
+	}, []);
+
+	const backgroundOptionOnDragOver = { "color": "dark-4", "opacity": true };
+	const inputElementStyle: React.CSSProperties = { display: "block", opacity: 0, zIndex: 10, position: "absolute", width: "100%", height: "100%" };
 	return (
 		<Box>
 			<Heading level="2">Import Images</Heading>
 			<Paragraph>First, import images which you want to process.</Paragraph>
-			<FileInput
-				multiple
-				onChange={onFileInputChange}
-			/>
-			<Box pad="small" overflow={props.overflow}>
+			<Box pad="small" overflow={props.overflow} ref={dropAreaRef} background={dragOver ? backgroundOptionOnDragOver : ""}>
 				<Grid
 					gap="small"
 					rows="small"
 					columns={{ count: 'fit', size: ['small', 'small'] }}
 				>
+					<Box
+						round
+						border
+						width="small"
+						height="small"
+						align="center"
+						justify="center"
+						hoverIndicator={{
+							background: {
+								color: 'background-contrast',
+							},
+							elevation: 'medium',
+						}}
+						onClick={() => { }}
+						key="add new image"
+					>
+						<Gallery size="large" />
+						<Text>Click or Drop files here</Text>
+						<input type="file" accept="image/png,image/jpeg,image/gif" onChange={onFileInputChange} style={inputElementStyle} />
+					</Box>
 					{images.length > 0 && images.map(image => (
 						<LoadedImage src={image.src} name={image.name} id={image.id} key={image.name} />
 					))}
