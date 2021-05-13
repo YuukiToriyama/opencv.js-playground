d3a1b50418cef9d07b58b079263110c6b48f49d7
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-13 13:18:38 +0900

    scrollbarが二重に表示される問題を解決

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import CustomFooter from "./layout/CustomFooter";
 const App: React.FunctionComponent = () => {
 	const [isOpencvLoaded, setIsOpencvLoaded] = React.useState(false);
 	return (
-		<Grommet theme={grommet} full>
+		<Grommet theme={grommet}>
 			<OpenCvProvider openCvPath="https://docs.opencv.org/4.5.0/opencv.js" onLoad={() => setIsOpencvLoaded(true)}>
 				<AppBar />
 				<MainContent />
