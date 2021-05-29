44f5f3a016d28fa5cf51c9bdcdabe27dce009750
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-30 02:35:25 +0900

    use mirada for loading opencv.js

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,33 @@ import {
 	Grommet
 } from 'grommet';
 import { grommet } from "grommet/themes/grommet";
-import { OpenCvProvider } from "opencv-react";
+import * as Mirada from "mirada";
 
 import AppBar from "./layout/AppBar";
 import MainContent from "./layout/MainContent";
 import CustomFooter from "./layout/CustomFooter";
 
+declare var cv: Mirada.CV;
+
 const App: React.FunctionComponent = () => {
 	const [isOpencvLoaded, setIsOpencvLoaded] = React.useState(false);
+	React.useEffect(() => {
+		Mirada.loadOpencv({
+			opencvJsLocation: "https://docs.opencv.org/4.5.0/opencv.js", // The location of opencv.js file to load.
+			force: true, // It will force the library loading - reloading it if it's already loaded
+		}).then(() => {
+			setIsOpencvLoaded(true);
+			console.log(cv.getBuildInformation());
+		}).catch(error => {
+			console.error(error);
+			setIsOpencvLoaded(false);
+		});
+	}, []);
 	return (
 		<Grommet theme={grommet}>
-			<OpenCvProvider openCvPath="https://docs.opencv.org/4.5.0/opencv.js" onLoad={() => setIsOpencvLoaded(true)}>
-				<AppBar />
-				<MainContent />
-				<CustomFooter />
-			</OpenCvProvider>
+			<AppBar />
+			<MainContent />
+			<CustomFooter />
 		</Grommet>
 	)
 }
